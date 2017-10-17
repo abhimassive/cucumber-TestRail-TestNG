086e02d9c168937262d69b7beb795ce8776d1151
abhimassive <abhijeet.daspatnaik@massive.co>
2017-10-17 11:28:21 +1100

    Working with TestNG and Cucumber Reports

--- Auto/src/main/java/listeners/TestListener.java
+++ Auto/src/main/java/listeners/TestListener.java
@@ -1,196 +1,246 @@
 package listeners;
 
-import business.Common;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestNGMethod;
-import org.testng.ITestResult;
-import testrail.TestRail;
-import testrail.TestRailAPI;
-
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class TestListener implements ITestListener {
-    // shows more detailed logs in console if set to true
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestNGMethod;
+import org.testng.ITestResult;
+import org.testng.Reporter;
 
-    Boolean debugInfo = true;
+import com.gargoylesoftware.htmlunit.javascript.configuration.BrowserName;
 
-    // runs any time a test starts
-    public void onTestStart(ITestResult arg0) {
-        String timeStamp = new SimpleDateFormat("dd/MM/yyy HH:mm:ss").format(Calendar.getInstance().getTime());
+import business.Common;
+import testbase.TestBase;
+import testrail.TestRail;
+import testrail.TestRailAPI;
 
-        ITestNGMethod m = arg0.getMethod();
+public class TestListener extends TestBase implements ITestListener {
+	// shows more detailed logs in console if set to true
 
-        // get test method name
-        String methodName = m.getMethodName();
-        // get test browser name
-        String browserName = m.getXmlTest().getParameter("browserName");
+	Boolean debugInfo = true;
 
-        System.out.println("");
-        System.out.println("===============================================");
+	/*
+	 * WebDriver driver;
+	 * 
+	 * public TestListener(WebDriver driver) { this.driver = driver; }
+	 */
+	// runs any time a test starts
+	public void onTestStart(ITestResult arg0) {
+		String timeStamp = new SimpleDateFormat("dd/MM/yyy HH:mm:ss").format(Calendar.getInstance().getTime());
 
-        if (browserName == null) {
-            System.out.println("Test started - " + methodName + " at " + timeStamp);
-        } else {
-            System.out.println("Test started - " + methodName + " on browser " + browserName + " at " + timeStamp);
-        }
+		ITestNGMethod m = arg0.getMethod();
 
-        System.out.println("===============================================");
-        System.out.println("");
-    }
+		// get test method name
+		String methodName = m.getMethodName();
+		// get test browser name
+		String browser = m.getXmlTest().getParameter("browserName");
 
-    // runs any time a test passes
-    public void onTestSuccess(ITestResult result) {
-        System.out.println("");
-        System.out.println("===============================================");
+		System.out.println("");
+		System.out.println("===============================================");
 
-        // if you have chosen to record test results to TestRail
-        if (Common.postResultsToTestRail) {
-            ITestNGMethod m = result.getMethod();
-            // get the method name for the test
-            String methodName = m.getMethodName();
-            // get the browser name for the test
-            String browserName = m.getXmlTest().getParameter("browserName");
+		if (browser == null) {
+			System.out.println("Test started - " + methodName + " at " + timeStamp);
+		} else {
+			System.out.println("Test started - " + methodName + " on Browser - " + browser + " at " + timeStamp);
+		}
 
-            // get the TestRail Test Run Id from the TestNG.xml file
-            int testRailTestRunId = 0;
-            String testRailTestRunIdStr = m.getXmlTest().getParameter("testRailTestRunId");
-            // if the Test Run id parameter exists in the xml then store it as an int
-            if (testRailTestRunIdStr != null && !testRailTestRunIdStr.isEmpty()) {
-                testRailTestRunId = Integer.parseInt(testRailTestRunIdStr);
-            }
+		System.out.println("===============================================");
+		System.out.println("");
+	}
 
-            // get the TestRail Test Case id from the Test annonation description value
-            int[] testCaseIds = returnTestCaseId(result);
+	// runs any time a test passes
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("");
+		System.out.println("===============================================");
 
-            // if the Test Case Id and Test Run Id have both been set then try to add the
-            // test result to TestRail
+		// if you have chosen to record test results to TestRail
+		if (Common.postResultsToTestRail) {
+			ITestNGMethod m = result.getMethod();
+			// get the method name for the test
+			String methodName = m.getMethodName();
+			// get the browser name for the test
+			String browser = m.getXmlTest().getParameter("browserName");
 
-            if (testCaseIds != null && testRailTestRunId != 0) {
-                // the comment that will be added to the Test Run result
-                Object commentFromTest = result.getTestContext().getAttribute("Comment");
-                if (debugInfo)
-                    System.out.println("Comment attribute = " + commentFromTest);
+			// get the TestRail Test Run Id from the TestNG.xml file
+			int testRailTestRunId = 0;
+			String testRailTestRunIdStr = m.getXmlTest().getParameter("testRailTestRunId");
+			// if the Test Run id parameter exists in the xml then store it as an int
+			if (testRailTestRunIdStr != null && !testRailTestRunIdStr.isEmpty()) {
+				testRailTestRunId = Integer.parseInt(testRailTestRunIdStr);
+			}
 
-                String additionalComment;
-                if (commentFromTest == null) {
-                    additionalComment = "";
-                } else {
-                    additionalComment = "\n\nAdditional info - " + commentFromTest.toString();
-                }
+			// get the TestRail Test Case id from the Test annonation description value
+			int[] testCaseIds = returnTestCaseId(result);
 
-                String testRailComment = "Automated TestNG Test - PASS; \n\nTest method name = " + methodName
-                        + "\nBrowser name = " + browserName + additionalComment;
-                System.out.println(testRailComment);
+			// if the Test Case Id and Test Run Id have both been set then try to add the
+			// test result to TestRail
 
-                try {
-                    // add the result to TestRail
-                    for (int testCaseId : testCaseIds) {
-                        System.out.println(testCaseId);
-                        TestRailAPI.addResult(1, testRailComment, testRailTestRunId, testCaseId);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else {
-                // if the Test Case Id or Test Run Id have not been set correctly record a
-                // message to the log
-                System.out.println("Test results are not being recorded to TestRail for test " + methodName
-                        + " on browser " + browserName);
-            }
-        }
-        System.out.println("===============================================");
-    }
+			if (testCaseIds != null && testRailTestRunId != 0) {
+				// the comment that will be added to the Test Run result
+				Object commentFromTest = result.getTestContext().getAttribute("Comment");
+				if (debugInfo)
+					System.out.println("Comment attribute = " + commentFromTest);
 
-    // runs any time a test fails
-    public void onTestFailure(ITestResult arg0) {
-        System.out.println("");
-        System.out.println("===============================================");
-        // store the failure exception
-        String errorMessage = arg0.getThrowable().toString();
+				String additionalComment;
+				if (commentFromTest == null) {
+					additionalComment = "";
+				} else {
+					additionalComment = "\n\nAdditional info - " + commentFromTest.toString();
+				}
 
-        // if you have chosen to record test results to TestRail
-        if (Common.postResultsToTestRail) {
-            ITestNGMethod m = arg0.getMethod();
-            // store the test method name
-            String methodName = m.getMethodName();
+				String testRailComment = "Automated TestNG Test - PASS; \n\nTest method name = " + methodName
+						+ "\nBrowser name = " + browser + additionalComment;
+				System.out.println(testRailComment);
 
-            // store the test browser name
-            String browserName = m.getXmlTest().getParameter("browserName");
+				try {
+					// add the result to TestRail
+					for (int testCaseId : testCaseIds) {
+						System.out.println(testCaseId);
+						TestRailAPI.addResult(1, testRailComment, testRailTestRunId, testCaseId);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				// if the Test Case Id or Test Run Id have not been set correctly record a
+				// message to the log
+				System.out.println("Test results are not being recorded to TestRail for test " + methodName
+						+ " on Browser - " + browser);
+			}
+		}
+		System.out.println("===============================================");
+	}
 
-            // get the TestRail Test Run Id from the TestNG.xml file
-            int testRailTestRunId = 0;
-            String testRailTestRunIdStr = m.getXmlTest().getParameter("testRailTestRunId");
-            // if the Test Run id parameter exists in the xml then store it as an int
-            if (testRailTestRunIdStr != null && !testRailTestRunIdStr.isEmpty()) {
-                testRailTestRunId = Integer.parseInt(testRailTestRunIdStr);
-            }
+	// runs any time a test fails
+	public void onTestFailure(ITestResult arg0) {
+		// Object currentClass = arg0.getInstance();
+		// WebDriver driver = ((TestBase) currentClass).getDriver();
+		System.out.println("");
+		System.out.println("===============================================");
+		// store the failure exception
+		String errorMessage = arg0.getThrowable().toString();
 
-            // get the TestRail Test Case id from the Test annonation description value
-            int[] testCaseIds = returnTestCaseId(arg0);
+		// if you have chosen to record test results to TestRail
+		if (Common.postResultsToTestRail) {
+			ITestNGMethod m = arg0.getMethod();
+			// store the test method name
+			String methodName = m.getMethodName();
 
-            // if the Test Case Id and Test Run Id have both been set then try to add the
-            // test result to TestRail
-            if (testCaseIds != null && testRailTestRunId != 0) {
-                // the comment that will be added to the Test Run result
-                Object commentFromTest = arg0.getTestContext().getAttribute("Comment");
-                if (debugInfo)
-                    System.out.println("Comment attribute = " + commentFromTest);
+			// store the test browser name
+			String browser = m.getXmlTest().getParameter("browserName");
 
-                String additionalComment;
-                if (commentFromTest == null) {
-                    additionalComment = "";
-                } else {
-                    additionalComment = "\nAdditional info - " + commentFromTest.toString();
-                }
-                String testRailComment = "Automated TestNG Test - FAIL\n\nTest method name = " + methodName
-                        + "\nBrowser name = " + browserName + "\n\nFailure Exception = " + errorMessage
-                        + additionalComment;
+			// get the TestRail Test Run Id from the TestNG.xml file
+			int testRailTestRunId = 0;
+			String testRailTestRunIdStr = m.getXmlTest().getParameter("testRailTestRunId");
+			// if the Test Run id parameter exists in the xml then store it as an int
+			if (testRailTestRunIdStr != null && !testRailTestRunIdStr.isEmpty()) {
+				testRailTestRunId = Integer.parseInt(testRailTestRunIdStr);
+			}
 
-                try {
-                    // add the result to TestRail
-                    for (int testCaseId : testCaseIds) {
-                        System.out.println(testCaseId);
-                        TestRailAPI.addResult(5, testRailComment, testRailTestRunId, testCaseId);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else {
-                // if the Test Case Id or Test Run Id have not been set correctly record a
-                // message to the log
-                System.out.println("Test results are not being recorded to TestRail for test " + methodName
-                        + " on browser " + browserName);
-            }
-        }
-        System.out.println("===============================================");
-    }
+			// get the TestRail Test Case id from the Test annonation description value
+			int[] testCaseIds = returnTestCaseId(arg0);
 
-    public void onTestSkipped(ITestResult arg0) {
-    }
+			// if the Test Case Id and Test Run Id have both been set then try to add the
+			// test result to TestRail
+			if (testCaseIds != null && testRailTestRunId != 0) {
+				// the comment that will be added to the Test Run result
+				Object commentFromTest = arg0.getTestContext().getAttribute("Comment");
+				if (debugInfo)
+					System.out.println("Comment attribute = " + commentFromTest);
 
-    public void onFinish(ITestContext arg0) {
-    }
+				String additionalComment;
+				if (commentFromTest == null) {
+					additionalComment = "";
+				} else {
+					additionalComment = "\nAdditional info - " + commentFromTest.toString();
+				}
+				String testRailComment = "Automated TestNG Test - FAIL\n\nTest method name = " + methodName
+						+ "\nBrowser name = " + browser + "\n\nFailure Exception = " + errorMessage + additionalComment;
 
-    public void onStart(ITestContext arg0) {
-    }
+				try {
+					// add the result to TestRail
+					for (int testCaseId : testCaseIds) {
+						System.out.println(testCaseId);
+						TestRailAPI.addResult(5, testRailComment, testRailTestRunId, testCaseId);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				// if the Test Case Id or Test Run Id have not been set correctly record a
+				// message to the log
+				System.out.println("Test results are not being recorded to TestRail for test " + methodName
+						+ " on Browser - " + browser);
+			}
+		}
+		System.out.println("===============================================");
+		// System.out.println(browser);
 
-    public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
-    }
+		boolean testPassed = arg0.isSuccess();
+		if (testPassed == false) {
+			Calendar calendar = Calendar.getInstance();
+			SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY_MM_dd_HH_mm_ss");
+			ITestNGMethod m = arg0.getMethod();
+			// store the test method name
+			String methodName = m.getMethodName();
 
-    public int[] returnTestCaseId(ITestResult result) {
-        ITestNGMethod testNGMethod = result.getMethod();
-        Method method = testNGMethod.getConstructorOrMethod().getMethod();
-        TestRail testRailAnnotation = method.getAnnotation(TestRail.class);
-        int[] testCaseIds;
-        try {
-            testCaseIds = testRailAnnotation.testCaseId();
-        } catch (Exception ex) {
-            return null;
-        }
-        return testCaseIds;
-    }
+			// store the test browser name
+			String browser = m.getXmlTest().getParameter("browserName");
+
+			File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			// System.out.println(browser);
+
+			try {
+
+				String reportDirectory = new File(System.getProperty("user.dir")).getAbsolutePath()
+						+ "/test-output/failure_screenshots/";
+				File destFile = new File(reportDirectory + methodName + "_" + browser + "_"
+						+ dateFormat.format(calendar.getTime()) + ".png");
+
+				FileUtils.copyFile(scrFile, destFile);
+
+				Reporter.log("<a href='" + destFile.getAbsolutePath() + "'> <img src='" + destFile.getAbsolutePath()
+						+ "' height='100' width='100'/> </a>");
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	public void onTestSkipped(ITestResult arg0) {
+	}
+
+	public void onFinish(ITestContext arg0) {
+	}
+
+	public void onStart(ITestContext arg0) {
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+	}
+
+	public int[] returnTestCaseId(ITestResult result) {
+		ITestNGMethod testNGMethod = result.getMethod();
+		Method method = testNGMethod.getConstructorOrMethod().getMethod();
+		TestRail testRailAnnotation = method.getAnnotation(TestRail.class);
+		int[] testCaseIds;
+		try {
+			testCaseIds = testRailAnnotation.testCaseId();
+		} catch (Exception ex) {
+			return null;
+		}
+		return testCaseIds;
+	}
 
 }
--- Auto/src/test/java/features/Login.feature
+++ Auto/src/test/java/features/Login.feature
@@ -5,13 +5,14 @@ Feature: Login
 
   Scenario Outline: As a user I want to login to Axis Responsive Web Application
     Given I navigate to Axis RWA
-    When I click on Sign In
+    And I click on Sign In
     And I Login with incorrect Email and Password
-    Then I should see an Unsuccessful login message: "Looks like your email or password is incorrect. Please try again."
+    Then I should see an Unsuccessful login message: "Looks like your email or password is incorrect. Please try again." 😲
     But When I Login with correct "<email>" and "<password>"
-    Then I should Successfully login and see my account badge with the first letter of my "<email>" in uppercase
+    Then I should Successfully login and see my account badge with the first letter of my "<email>" in uppercase 😃
 
 
     Examples:
       | email                          | password  |
       | abhijeet.daspatnaik@massive.co | ABHI@dp11 |
+      | 4profiles@profile.com          | password  |
