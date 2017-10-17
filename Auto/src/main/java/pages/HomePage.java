79d9dad16f14f83667da7719e7794c4d66b5f88c
abhidp <abhi358@gmail.com>
2017-10-17 11:28:21 +1100

    Working with TestNG and Cucumber Reports

--- Auto/src/main/java/pages/HomePage.java
+++ Auto/src/main/java/pages/HomePage.java
@@ -1,13 +1,15 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import testbase.TestBase;
 
-public class HomePage {
+import java.util.List;
 
-    //WebDriver driver;
+public class HomePage extends TestBase {
 
     @FindBy(linkText = "Sign In")
     public WebElement signIn;
@@ -17,14 +19,16 @@ public class HomePage {
     public WebElement login_password;
     @FindBy(xpath = "//button[@class='account-btn__input account-btn__input--primary sign-in-form__sign-in']")
     public WebElement login_button;
-
     @FindBy(css = ".profile-icon__initial.uppercase")
     public WebElement profile_icon;
-
     @FindBy(css = ".sign-in-form__error")
-    public WebElement actual_login_error_msg;
+    public WebElement login_error_msg;
+    @FindBy(xpath = "//div[@class='profile-circle__letter uppercase']")
+    public WebElement profiles_circle;
+    WebDriver driver;
 
     public HomePage(WebDriver driver) {
+        this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
@@ -32,47 +36,36 @@ public class HomePage {
         signIn.click();
     }
 
-
-    public void enterCredenials(String email_id, String password) {
-        login_email.sendKeys(email_id);
-        login_password.sendKeys(password);
-    }
-
     public void clickLoginButton() {
         login_button.submit();
+    }
+
+    public String whosWatching() {
+
+        boolean found = driver.findElement(By.xpath("//span[@class='profile-icon__initial uppercase']")).isDisplayed();
+
+        if (found == false) {
+            List<WebElement> listOfWatchers = driver.findElements(By.xpath("//div[@class='profile-circle__letter uppercase']"));
+            listOfWatchers.get(0).click();
+            getProfileIconLetter();
+
+        } else {
+            getProfileIconLetter();
+        }
+
+        return getProfileIconLetter();
     }
 
     public String getProfileIconLetter() {
         return profile_icon.getText();
     }
 
-    public String actualLoginErrorMessage() {
-        return actual_login_error_msg.getText();
-    }
-/*
-       profile
-        return "abcd";
+    public String actualLoginErrorMessage() throws InterruptedException {
+
+        return login_error_msg.getText();
+
 
     }
-
-    public String getLoginErrorMsg() {
-
-       /*WebDriverWait wait = new WebDriverWait(driver, 30);
-
-        try {
-            login_error = wait
-                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//p[@class='sign-in-form__error']")));
-
-            String error_msg_txt = login_error.getText();
-
-            System.out.println("Actual Error Message is : " + error_msg_txt);
-            return error_msg_txt;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return e.getMessage();
-        }*/
-
 }
 
 
--- Auto/src/main/java/testbase/TestBase.java
+++ Auto/src/main/java/testbase/TestBase.java
@@ -1,51 +1,207 @@
 package testbase;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
     public WebDriver driver;
-    //public ITestResult result;
-    String browser = "firefox";
+    public String nodeURL;
+    //TestListener lis;
 
-    public void init() {
-        selectBrowser(browser);
+    public WebDriver getDriver() {
+        return driver;
     }
 
-    /*public WebDriver getDriver() {
-        return driver;
-    }*/
 
-    public void selectBrowser(String browser) {
+    //String browserName = "grid";
+
+    public void setupTest() {
+        String browser = null;
+        try {
+            File fXmlFile = new File("/Users/abhi/Auto/Axis_Sprint_1.xml");
+            //File fXmlFile = new File(System.getProperty("user.dir") + "/Axis_Sprint_1.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+
+            doc.getDocumentElement().normalize();
+            NodeList nodeList = doc.getElementsByTagName("parameter");
+
+            System.out.println("----------------------------");
+
+            for (int temp = 0; temp < nodeList.getLength(); temp++) {
+
+                Node nNode = nodeList.item(temp);
+
+                //System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element eElement = (Element) nNode;
+                    if (eElement.getAttribute("name").equalsIgnoreCase("browserName")) {
+                        browser = eElement.getAttribute("value");
+                        break;
+                    }
+
+                }
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        init(browser);
+    }
+
+
+    public void init(String brow) {
+        //lis = new TestListener(driver);
+        selectBrowser(brow);
+    }
+
+    public void selectBrowser(String browserName) {
         if (System.getProperty("os.name").contains("Window")) {
-            if (browser.equals("chrome")) {
-                System.out.println(System.getProperty("user.dir"));
+            if (browserName.equalsIgnoreCase("chrome")) {
                 System.setProperty("webdriver.chrome.driver",
                         System.getProperty("user.dir") + "/drivers/chromedriver.exe");
                 driver = new ChromeDriver();
-            } else if (browser.equals("firefox")) {
+            } else if (browserName.equalsIgnoreCase("firefox")) {
                 System.out.println(System.getProperty("user.dir"));
                 System.setProperty("webdriver.gecko.driver",
                         System.getProperty("user.dir") + "/drivers/geckodriver.exe");
                 driver = new FirefoxDriver();
             }
         } else if (System.getProperty("os.name").contains("Mac")) {
-            if (browser.equals("chrome")) {
-                System.out.println(System.getProperty("user.dir"));
+            if (browserName.equalsIgnoreCase("chrome")) {
                 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
                 driver = new ChromeDriver();
-            } else if (browser.equals("firefox")) {
+            } else if (browserName.equalsIgnoreCase("firefox")) {
                 System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/drivers/geckodriver");
                 driver = new FirefoxDriver();
+            } else if (browserName.equalsIgnoreCase("opera")) {
+                System.setProperty("webdriver.opera.driver", System.getProperty("user.dir") + "/drivers/operadriver");
+                driver = new OperaDriver();
+            } else if (browserName.equalsIgnoreCase("safari")) {
+                driver = new SafariDriver();
+            } else if (browserName.equalsIgnoreCase("phantom")) {
+                System.setProperty("phantomjs.binary.path", System.getProperty("user.dir") + "/drivers/phantomjs");
+                driver = new PhantomJSDriver();
+            } else if (browserName.equalsIgnoreCase("HeadlessChrome")) {
+                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("headless");
+                driver = new ChromeDriver(options);
+            } else if (browserName.equalsIgnoreCase("unit")) {
+                java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(java.util.logging.Level.OFF);
+                java.util.logging.Logger.getLogger("org.apache.http").setLevel(java.util.logging.Level.OFF);
+                driver = new HtmlUnitDriver();
+            } else if (browserName.equalsIgnoreCase("grid")) {
+                try {
+                    nodeURL = "http://10.0.2.111:5555/wd/hub";
+                    DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+                    capabilities.setBrowserName("chrome");
+                    capabilities.setPlatform(Platform.MAC);
+                    driver = new RemoteWebDriver(new URL(nodeURL), capabilities);
+                    System.out.println("DRIVER  " + driver);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
             }
         }
+
+        driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
     }
+
+    /*public void waitForElement(WebDriver driver, int timeOutInSeconds, WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }*/
+
+    public WebElement waitForElement(WebDriver driver, WebElement element, long timeOutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.until(ExpectedConditions.visibilityOf(element));
+        return element;
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     /* Cucumber Setup */
@@ -56,23 +212,23 @@ public class TestBase {
         System.out.println(System.getProperty("os.name"));
 
         if (System.getProperty("os.name").contains("Window")) {
-            if (browser.equals("chrome")) {
+            if (browserName.equals("chrome")) {
                 System.out.println(System.getProperty("user.dir"));
                 System.setProperty("webdriver.chrome.driver",
                         System.getProperty("user.dir") + "/drivers/chromedriver.exe");
                 driver = new ChromeDriver();
-            } else if (browser.equals("firefox")) {
+            } else if (browserName.equals("firefox")) {
                 System.out.println(System.getProperty("user.dir"));
                 System.setProperty("webdriver.gecko.driver",
                         System.getProperty("user.dir") + "/drivers/geckodriver.exe");
                 driver = new FirefoxDriver();
             }
         } else if (System.getProperty("os.name").contains("Mac")) {
-            if (browser.equals("chrome")) {
+            if (browserName.equals("chrome")) {
                 System.out.println(System.getProperty("user.dir"));
                 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
                 driver = new ChromeDriver();
-            } else if (browser.equals("firefox")) {
+            } else if (browserName.equals("firefox")) {
                 System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/drivers/geckodriver");
                 driver = new FirefoxDriver();
             }
@@ -93,4 +249,4 @@ public class TestBase {
     }*/
 
 
-}
+
