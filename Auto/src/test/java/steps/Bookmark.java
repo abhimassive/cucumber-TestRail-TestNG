05263ff5b400c81e533d331da3ac1767a6ec1f1e
abhidp <abhi358@gmail.com>
2017-10-19 00:40:39 +1100

    bookmark

---
+++ Auto/src/test/java/steps/Bookmark.java
@@ -0,0 +1,17 @@
+package steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import pages.HomePage;
+import testbase.TestBase;
+
+public class Bookmark extends TestBase {
+    HomePage homepage;
+
+    @Given("^I am logged in into AXIS RWA$")
+    public void iAmLoggedIn() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
+
